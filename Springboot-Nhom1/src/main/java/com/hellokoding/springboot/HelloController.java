a6b5e7762962cb84c088a98b6512fe235284e7e6
winterlovet43 <14110387@student.hcmute.edu.vn>
2017-04-26 09:33:10 +0700

    Update HelloController.java

--- Springboot-Nhom1/src/main/java/com/hellokoding/springboot/HelloController.java
+++ Springboot-Nhom1/src/main/java/com/hellokoding/springboot/HelloController.java
@@ -12,6 +12,7 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello";
     }
+    @RequestMapping("/body1")
     public String hello1(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
         return "body1";
